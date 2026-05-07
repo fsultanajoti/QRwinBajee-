f9daffdc91543aedb84146c774d590dc8b962e0d
fsultanajoti <farjanasultanajoti@gmail.com>
2026-05-07 16:47:18 +0600

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const router = express.Router();
 
+// Demo login
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === "admin" && password === "1234") {
+        res.json({ success: true, token: "admin-token" });
+    } else {
+        res.json({ success: false, message: "Invalid login" });
+    }
+});
+
+module.exports = router;
