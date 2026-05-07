a4771476b5c73faa0e1808993869a9300d54867e
fsultanajoti <farjanasultanajoti@gmail.com>
2026-05-07 16:46:53 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/user");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
+
+app.get("/", (req, res) => {
+    res.send("QrWinBajee Backend Running 🚀");
+});
+
+const PORT = 6700;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
