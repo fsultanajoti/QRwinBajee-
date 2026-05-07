cd7898d785b0107deb3033ca3cdc303d551163e2
fsultanajoti <farjanasultanajoti@gmail.com>
2026-05-07 16:46:40 +0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,18 @@
+const mysql = require("mysql2");
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "qrwinbajee"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("DB Connection Failed ❌", err);
+    } else {
+        console.log("Database Connected ✅");
+    }
+});
+
+module.exports = db;
