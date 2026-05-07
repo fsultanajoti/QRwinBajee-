ebccca2143483b7fd9df2805a9d46115886aae6d
fsultanajoti <farjanasultanajoti@gmail.com>
2026-05-07 16:06:47 +0600

    Create admin.js

---
+++ admin/admin.js
@@ -0,0 +1,14 @@
+function login() {
+  const u = document.getElementById("user").value;
+  const p = document.getElementById("pass").value;
+
+  if (u === "admin" && p === "1234") {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("❌ Wrong credentials");
+  }
+}
+
+function saveSettings() {
+  alert("✅ Settings saved (demo)");
+}
